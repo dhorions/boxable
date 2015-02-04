2c90073f491c94fcded9c467c365e651968fd0a0
David Gautier <dg.david.gautier@gmail.com>
2015-02-04 19:08:53 +0100

    Removed : throwing of IOException replaced by IllegalArgumentException

--- src/main/java/be/quodlibet/boxable/Paragraph.java
+++ src/main/java/be/quodlibet/boxable/Paragraph.java
@@ -15,18 +15,16 @@ import java.util.List;
 public class Paragraph {
 
 
-    
     private int width = 500;
 
-    
+
     private String text;
     private PDFont font = PDType1Font.HELVETICA;
     private int fontSize = 10;
 
     private int color = 0;
 
-    public Paragraph(String text, PDFont font, int fontSize, int width)
-    {
+    public Paragraph(String text, PDFont font, int fontSize, int width) {
         this.text = text;
         this.font = font;
         this.fontSize = fontSize;
@@ -34,23 +32,27 @@ public class Paragraph {
     }
 
 
-    public List<String> getLines() throws IOException
-    {
+    public List<String> getLines() {
         List<String> result = new ArrayList();
 
         String[] split = text.split("(?<=\\W)");
         int[] possibleWrapPoints = new int[split.length];
         possibleWrapPoints[0] = split[0].length();
-        for ( int i = 1 ; i < split.length ; i++ ) {
-            possibleWrapPoints[i] = possibleWrapPoints[i-1] + split[i].length();
+        for (int i = 1; i < split.length; i++) {
+            possibleWrapPoints[i] = possibleWrapPoints[i - 1] + split[i].length();
         }
 
         int start = 0;
         int end = 0;
-        for ( int i : possibleWrapPoints ) {
-            float width = font.getStringWidth(text.substring(start,i)) / 1000 * fontSize;
-            if ( start < end && width > this.width ) {
-                result.add(text.substring(start,end));
+        for (int i : possibleWrapPoints) {
+            float width = 0;
+            try {
+                width = font.getStringWidth(text.substring(start, i)) / 1000 * fontSize;
+            } catch (IOException e) {
+                throw new IllegalArgumentException(e.getMessage(), e);
+            }
+            if (start < end && width > this.width) {
+                result.add(text.substring(start, end));
                 start = end;
             }
             end = i;
@@ -63,6 +65,7 @@ public class Paragraph {
     public float getFontHeight() {
         return font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * fontSize;
     }
+
     public float getFontWidth() {
         return font.getFontDescriptor().getFontBoundingBox().getWidth() / 1000 * fontSize;
     }
