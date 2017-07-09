5220a2e0cfb1d2cf9e80314f7b1c51017f898fea
christoph <christoph@christoph-Lenovo-G560>
2017-07-09 16:52:53 +0200

    add documentation

--- src/main/java/be/quodlibet/boxable/datatable/updateCellProperty.java
+++ src/main/java/be/quodlibet/boxable/datatable/updateCellProperty.java
@@ -4,6 +4,11 @@ import org.apache.pdfbox.pdmodel.PDPage;
 
 import be.quodlibet.boxable.Cell;
 
+/**
+ * Allows changing the cell properties, while the CSV documents is written directly into the PDF tables
+ *
+ * @author Christoph Schemmelmann {@code <CSchemmy@gmx.de>}
+ */
 public interface updateCellProperty {
 
 	abstract void updateCellPropertysAtColumn(Cell<PDPage> cell, int column, int row);
